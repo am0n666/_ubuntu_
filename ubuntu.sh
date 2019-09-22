d473ea7815e52bd094e41f0b5462de6fb1bc1ab4
am0n666 <bethor666@gmail.com>
2019-09-22 20:42:35 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -37,14 +37,13 @@ mkdir -p binds
 bin=start-ubuntu.sh
 echo "writing launch script"
 cat > $bin <<- EOM
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 cd \$(dirname \$0)
 ## unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
 command="proot"
 command+=" --link2symlink"
-command+=" -0"
-command+=" -r $folder"
+command+=" -S $folder"
 if [ -n "\$(ls -A binds)" ]; then
     for f in binds/* ;do
       . \$f
@@ -64,6 +63,7 @@ command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/us
 command+=" TERM=\$TERM"
 command+=" LANG=C.UTF-8"
 command+=" /bin/bash --login"
+export PROOT_NO_SECCOMP=1
 com="\$@"
 if [ -z "\$1" ];then
     exec \$command
